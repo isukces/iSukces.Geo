57728805db264fa8a3d88d0bf182793dad54ca15
Piotr Stęclik <piotr.steclik@isukces.com>
2023-06-09 11:46:11 +0200

    Dodaje rodzaje klasyfikacji punktów geodezyjnych

---
+++ App/iSukces.Geo/GeoPointClassification.cs
@@ -0,0 +1,17 @@
+﻿namespace iSukces.Geo
+{
+    public enum GeoPointClassification
+    {
+        Unknown,
+        Pipeline,
+        Electric,
+        Water,
+        // Kanalizacja - uzupełnić deszczowa/sanitarna
+        Sewers,
+        Telecommunication,
+        Gas,
+        Gasoline,
+        Oil,
+        Other
+    }
+}
